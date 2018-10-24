9826156fa6bdc006ec7a0bdedea3fb438332f44b
Julien Cohen-Adad <jcohen@polymtl.ca>
2018-10-24 15:52:00 -0400

    register_neighbor_slices.py: first commit

---
+++ scripts/register_neighbor_slices.py
@@ -0,0 +1,6 @@
+# #!/usr/bin/env python
+# 
+# This script loops across slices and registers slice i-1 on slice i and 
+# slice i+1 on slice i. Then, it applies the transformations on the neighbor 
+# slices and outputs a 3d volume (x, y, 3) for each metric.
+
