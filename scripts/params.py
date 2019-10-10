115ba4010d18fc02ccb494747112bee50bb57ef7
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-10-10 11:24:17 -0400

    params: Added file_prefix

--- scripts/params.py
+++ scripts/params.py
@@ -3,6 +3,9 @@
 # Description: Parameters for registration of tracts.
 
 
+# PARAMETERS THAT ARE SPECIFIC TO THE INPUT DATA
+# ======================================================================================================================
+
 FOLDER = '/Users/julien/Desktop/AtlasRat'  # Path to folder that contains all slices and all metrics
 OUTPUT_FOLDER = 'results'
 
@@ -51,37 +54,10 @@ regions = {
           "S4"],
     }
 
-# Cervical = ["C1",
-#             "C2",
-#             "C3",
-#             "C4",
-#             "C5",
-#             "C6",
-#             "C7",
-#             "C8"]
-#
-# Thoracic = ["T1",
-#             "T2",
-#             "T3",
-#             "T4",
-#             "T5",
-#             "T6",
-#             "T7",
-#             "T8",
-#             "T9",
-#             "T10",
-#             "T11",
-#             "T12",
-#             "T13"]
-#
-# Lumbar = ["L1",
-#           "L2",
-#           "L3",
-#           "L4",
-#           "L5",
-#           "L6"]
-#
-# Sacral = ["S1",
-#           "S2",
-#           "S3",
-#           "S4"]
+regions_ordered = ['cervical', 'thoracic', 'lumbar', 'sacral']
+
+
+# PARAMETERS THAT ARE USED INTERNALLY BY THE CODE
+# ======================================================================================================================
+
+file_prefix = 'AtlasRat_AllMetrics'
